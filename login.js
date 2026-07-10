42aaa206b663aedae947c8d8c8afc83afb62fa02
roudhotulbaniin-arch <roudhotulbaniin@gmail.com>
2026-07-11 06:41:01 +0700

    Update login.js
    Initial Upload

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
-import { auth } from "firebase.js";
-
+import { auth } from "./firebase.js";
 import {
     signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+
 document.getElementById("btnLogin").addEventListener("click", async (e) => {
 
     e.preventDefault();
@@ -10,12 +10,20 @@ document.getElementById("btnLogin").addEventListener("click", async (e) => {
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value;
 
-try {
-    await signInWithEmailAndPassword(auth, email, password);
+    try {
 
-    window.location.href = "dashboard.html";
+        alert("Login berhasil");
 
-} catch (e) {
-    alert(e.code + "\n\n" + e.message);
-}
+        await signInWithEmailAndPassword(auth, email, password);
+
+        alert("Akan pindah ke dashboard");
+
+        window.location.href = "dashboard.html";
+
+    } catch (e) {
+
+        alert(e.code + "\n\n" + e.message);
+
+    }
+
 });
