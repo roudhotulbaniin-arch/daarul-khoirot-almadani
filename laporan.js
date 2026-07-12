21cd596ec02498fe823c1f27726a8603b25aae06
roudhotulbaniin-arch <roudhotulbaniin@gmail.com>
2026-07-12 10:28:28 +0700

    Update laporan.js
    Initial Upload

--- laporan.js
+++ laporan.js
@@ -115,7 +115,7 @@ set("avgTajwidGabungan", formatNilai(tajwid));
 
 set("avgTotalGabungan", formatNilai(predikat.angka));
 set("avgTotalAngka", formatNilai(predikat.angka));
-set("avgTotalHuruf", predikat.arab);
+set("avgTotalHuruf", predikat.latin);
 set("avgTotalArab", predikat.tulisan);
 
 // ======================
